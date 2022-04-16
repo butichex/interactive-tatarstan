e78a1fa46c9d34f09def03137ead6453a34dbde4
butichex <tdyutin@gmail.com>
2022-04-16 12:33:30 +0300

    Инициализирован проект

---
+++ scripts/constants.js
@@ -0,0 +1,23 @@
+var constants = {
+	// Storymapap Info
+  _mapTitle: 'Interactive Tatarstan Title',
+  _mapSubtitle: 'Interactive Tatarstan Subtitle',
+  _mapLogo: 'Interactive Tatarstan Logo',
+  // Credits
+	_authorName: 'Имя автора:',
+	_authorURL: 'Почта автора или веб-сайт:',
+  _codeCredit: 'Автор кода:',
+	_githubRepo: 'Автор репозитория GitHub',
+	// Map Settings
+	_tileProvider: 'Basemap Tiles',
+	_zoomControls: 'Zoom Controls',
+  _narrativeWidth: 'Narrative Width',
+  _imgContainerHeight: 'Media Container Height',
+  _pixelsAfterFinalChapter: 'Pixels After Final Chapter',
+  _narrativeBackground: 'Narrative Background Color',
+  _narrativeText: 'Narrative Text Color',
+  _narrativeActive: 'Active Chapter Background Color',
+  _narrativeLink: 'Narrative Link Color',
+  _googleAnalytics: 'Google Analytics Tracking ID',
+  _enableLightbox: 'Enable Lightbox for Images'
+};
